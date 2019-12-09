80494a6a540c2d4ff14e5b329fe6e3aad88133f4
SIDDHARTH KAUSHIK <siddharth9k9@gmail.com>
2019-12-09 10:55:48 -0800

    commit

--- graphColoring.cpp
+++ graphColoring.cpp
@@ -1,1 +1,105 @@
+#include<iostream>
+#include<map>
+#include<list>
+#include<queue>
+using namespace std;
+template<typename T>
+class Graph{
+    map<T,list<T>> edge;
+public:
+    void addEdge(T u,T v,bool isBirectional=true){
+        edge[u].push_back(v);
+        if(isBirectional){
+            edge[v].push_back(u);
+        }
+    }
+   bool chooseColor(T node,T v, map<T,int> &color){
+       int m = 0;
+       do{
+            int flag=0;
+            color[v] = (color[v]+1)%(m+1);
+            if(color[v]==0){
+                    m++;
+                //return false;
+            }
+            for(auto neighbour:edge[v]){
+                if(color[neighbour]==color[v]){
+                    flag=1;
+                    break;
+                }
+            }
+            if(flag==0){
+                return true;
+            }
 
+
+
+        }while(true);
+
+
+   }
+   void color(T src){
+
+       map<T,int> color;
+        queue<T> que;
+		map<T,bool> visited;
+		que.push(src);
+		 visited[src] = 1;
+       color[src] = 1;
+       int flag = 1;
+
+       while(!que.empty()){
+        T node = que.front();
+        que.pop();
+        for(auto neighbour:edge[node]){
+            if(visited[neighbour]==0){
+                visited[neighbour]=1;
+                que.push(neighbour);
+                if(chooseColor(node,neighbour,color)==false){
+                    cout<<"colors are to less increase the no of colors\n";
+                    flag=0;
+                }
+            }
+        }
+
+       }
+
+       if(flag==1){
+        for(auto a:color){
+        cout<<a.first<<"->"<<a.second<<endl;
+       }
+       }
+
+
+
+
+   }
+	void printGraph(){
+			for(auto i:edge){
+				cout<<i.first<<"->";
+				for(auto j:i.second){
+					cout<<j<<"->";
+				}
+				cout<<"\n";
+			}
+		}
+
+
+};
+int main(){
+	Graph<string> g;
+    g.addEdge("english","programming logic");
+    g.addEdge("english","cpp");
+    g.addEdge("programming logic","cpp");
+
+
+
+
+
+
+
+
+	g.color("english");
+	g.printGraph();
+	return 0;
+}
--- hamiltonianCycle2.cpp
+++ hamiltonianCycle2.cpp
@@ -3,42 +3,40 @@ using namespace std;
 #define v 5
 
 
-void printSolution(int path[])  
+void printSolution(int color[])  
 {  
     cout << "Solution Exists \n"   ;  
     for (int i = 0; i < v; i++)  
-        cout << path[i] << " ";   
+        cout << color[i] << " ";   
    
-    cout << path[0] << " ";  
+    cout << color[0] << " ";  
     cout << endl; 
 }  
   
   
-bool isSafe(bool graph[v][v],int *path,int vertex,int position){
-	if(graph[path[position-1]][vertex]==0){
-		return false;
-	}
-	for(int i=0;i<position;i++ ){
-		if(path[i]==vertex){
+bool isSafe(bool graph[v][v],int *color,int vertex,int c){
+
+	for(int i=0;i<v;i++ ){
+		if(graph[vertex][i]!=0 and color[i]==c){
 			return false;
 		}
 	}
 	return true;
 }
 
-bool hamiltonianCycleUtility(bool graph[v][v],int *path,int position){
-	if(position==v){		
+bool hamiltonianCycleUtility(bool graph[v][v],int *path,int vertex,int c){
+	if(vertex==v){
 		printSolution(path);			
 		}
 	
 	for(int i=0;i<v;i++){
-		if(isSafe(graph,path,i,position)){
-			path[position]=i;
-			int next = hamiltonianCycleUtility(graph,path,position+1);
+		if(isSafe(graph,path,i,c)){
+			path[vertex]=i;
+			int next = hamiltonianCycleUtility(graph,path,i+1,i+1);
 			if(!next){
 				return false;
 			}
-			path[position] = -1;
+			path[vertex] = -1;
 		}
 	}
 	return true;
@@ -53,10 +51,10 @@ int main(){
     int *path = new int[v];  
     for (int i = 0; i < v; i++)  
         path[i] = -1;  
-   path[0] = 0;  
+  // path[0] = 0;  
   
    
-    hamiltonianCycleUtility(graph1, path,1) ;
+    hamiltonianCycleUtility(graph1, path,0,0) ;
      
   
     
